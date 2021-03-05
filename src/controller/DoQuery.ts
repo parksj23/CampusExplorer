ab9ee061edbdaadf45247f81c136a11f7b73a124
Sujin Park <spark23@students.cs.ubc.ca>
2021-03-05 14:18:11 -0800

    added multiple order keys case

--- src/controller/DoQuery.ts
+++ src/controller/DoQuery.ts
@@ -68,6 +68,22 @@ export default class DoQuery {
                 });
                 sections = ascending;
             }
+
+            if (Array.isArray(order)) { // if there is >1 key in order, then we sort by the last one
+                let lastOrderKey = order[order.length - 1];
+                order = lastOrderKey;
+                let ascending = sections.sort((a: any, b: any) => {
+                    if (a[order] < b[order]) {
+                        return -1;
+                    }
+                    if (a[order] > b[order]) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                });
+                sections = ascending;
+            }
         }
         return sections;
     }

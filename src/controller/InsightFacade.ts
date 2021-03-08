6d18f185c2e9257ed27063a2192c3102038ffbb1
Sujin Park <spark23@students.cs.ubc.ca>
2021-03-07 22:09:25 -0800

    got rid of a line in addDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -16,7 +16,8 @@ import ValidateQuery from "./ValidateQuery";
 import DoQuery from "./DoQuery";
 import {rejects} from "assert";
 import {expect} from "chai";
-import {Dataset} from "./Dataset";
+import Dataset from "./Dataset";
+import DatasetHelper from "./DatasetHelper";
 
 /**
  * This is the main programmatic entry point for the project.
@@ -30,6 +31,8 @@ export default class InsightFacade implements IInsightFacade {
     public memory: string[] = [];
     public addedDatasetContent: Dataset[] = [];
     public performQueryDatasetIds: string[] = [];
+
+    public d = new DatasetHelper();
 
     constructor() {
         Log.trace("InsightFacadeImpl::init()");
@@ -47,7 +50,7 @@ export default class InsightFacade implements IInsightFacade {
                 return reject(new InsightError("Invalid id."));
             }
             // check if already added
-            if (this.memory.includes(id)) {
+            if (this.d.memory.includes(id)) {
                 return reject(new InsightError("Dataset already added."));
             }
             let zip = new JSZip();
@@ -70,7 +73,7 @@ export default class InsightFacade implements IInsightFacade {
                         if (sectionType === "object") {
                             let objKeys = (Object.getOwnPropertyNames(section));
                             if (objKeys.includes("result")) {
-                                validSections = this.getSectionFields(section["result"], validSections);
+                                validSections = this.d.getSectionFields(section["result"], validSections);
                             }
                         }
                     }
@@ -80,8 +83,8 @@ export default class InsightFacade implements IInsightFacade {
                         try {
                             // both files saved before timeout? big
                             // small files, only one saves but passes
-                            this.saveData(id, InsightDatasetKind.Courses, validSections);
-                            return resolve(this.memory);
+                            this.d.saveData(id, InsightDatasetKind.Courses, validSections);
+                            return resolve(this.d.memory);
                         } catch (e) {
                             return reject(new InsightError("Data was parsed correctly but not saved"));
                         }
@@ -92,68 +95,6 @@ export default class InsightFacade implements IInsightFacade {
                 return reject(new InsightError("Non-zip folder."));
             });
         });
-    }
-
-    private saveData(id: string, kind: InsightDatasetKind, validSections: any[]): Promise<boolean> {
-        return new Promise<boolean>((resolve, reject) => {
-            let fs = require("fs");
-            let insightDataset: InsightDataset = {id, kind, numRows: validSections.length};
-            this.datasets.push(insightDataset);
-            this.memory.push(id);
-            let datasetContent = new Dataset(id, validSections);
-            this.addedDatasetContent.push(datasetContent);
-            const directory = "./src/data/";
-            const filePath: string = directory + id;
-            // TODO is the content okay for loading? we can also do more separate files. Do we also load memory ->
-            //  helper functions need access to class variables?
-            const content = JSON.stringify(datasetContent);
-            fs.promises.mkdir(directory, {recursive: true}).then(() => {
-                fs.promises.writeFile(filePath, content).then(() => {
-                    resolve();
-                });
-            });
-        });
-    }
-
-    private getSectionFields(result: any, sections: any[]): any[] {
-        let resultLength = result.length;
-        let initialDesiredFields: string[] = ["Avg", "Pass", "Fail", "Audit", "Year", "Subject", "Course", "Professor",
-            "Title", "id", "Section"];
-        if (!(resultLength < 1)) {
-            for (let i of result) {
-                let section: any = {};
-                let validSectionFields: any[] = [];
-                let initialSectionFields = Object.keys(i);
-                for (let key of initialDesiredFields) {
-                    let iKey = typeof i[key];
-                    if (initialSectionFields.includes(key)) {
-                        validSectionFields.push(i[key]);
-                    }
-                }
-                section["avg"] = i["Avg"];
-                section["pass"] = i["Pass"];
-                section["fail"] = i["Fail"];
-                section["audit"] = i["Audit"];
-                section["year"] = Number(i["Year"]);
-                if (i["Section"].toLowerCase() === "overall") {
-                    section["year"] = 1900;
-                }
-                section["dept"] = i["Subject"];
-                section["id"] = i["Course"];
-                section["instructor"] = i["Professor"];
-                section["title"] = i["Title"];
-                section["uuid"] = i["id"].toString();
-
-                if (typeof section["avg"] === "number" && typeof section["pass"] === "number"
-                    && typeof section["fail"] === "number" && typeof section["audit"] === "number"
-                    && typeof section["year"] === "number" && typeof section["dept"] === "string"
-                    && typeof section["id"] === "string" && typeof section["instructor"] === "string"
-                    && typeof section["title"] === "string" && typeof section["uuid"] === "string") {
-                    sections.push(section);
-                }
-            }
-        }
-        return sections;
     }
 
     public removeDataset(id: string):
@@ -168,14 +109,14 @@ export default class InsightFacade implements IInsightFacade {
                 return reject(new InsightError("Invalid id."));
             }
             try {
-                if (!(this.memory.includes(id))) {
+                if (!(this.d.memory.includes(id))) {
                     return reject(new NotFoundError("Dataset not found."));
                 } else {
-                    for (let index = 0; index < this.memory.length; index++) {
-                        if (this.memory[index] === id) {
-                            result = this.memory[index];
-                            this.memory.splice(index, 1);
-                            this.datasets.splice(index, 1);
+                    for (let index = 0; index < this.d.memory.length; index++) {
+                        if (this.d.memory[index] === id) {
+                            result = this.d.memory[index];
+                            this.d.memory.splice(index, 1);
+                            this.d.datasets.splice(index, 1);
                             let filepath: string = "data/" + id;
                             fs.unlink(filepath, (e: any) => {
                                 if (e) {
@@ -205,14 +146,7 @@ export default class InsightFacade implements IInsightFacade {
                     }
 
                     let queryingDatasetId = this.performQueryDatasetIds[0];
-                    let sections: any[] = [];
-                    for (let dataset of this.addedDatasetContent) {
-                        if (dataset.getDatasetId() === queryingDatasetId) {
-                            sections = dataset.getCoursesArray();
-                        }
-                    }
-                    let d = new Dataset(queryingDatasetId, sections);
-                    let data = d.getData(queryingDatasetId, this.memory);
+                    let data = this.d.getData(queryingDatasetId);
 
                     let doQuery = new DoQuery(query, data);
                     let resultArray = doQuery.doInitialQuery(query);
@@ -231,6 +165,6 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public listDatasets(): Promise<InsightDataset[]> {
-        return Promise.resolve(this.datasets);
+        return Promise.resolve(this.d.datasets);
     }
 }
